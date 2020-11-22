357773cb62c6e2a6575e65906479a6d2eb6a0ffd
hackelite01 <70997872+hackelite01@users.noreply.github.com>
2020-11-22 18:06:59 +0530

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -8,18 +8,21 @@ yellow='\e[1;33m'
 echo ""
 echo""
 banner() {
- echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██████  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██████  ██   ██ ██ ███████   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   █  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██   █  ██   ██ ██ ██        \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██   ██ ██    ██ ███\e[0m''\e[1;33m\e[0m\e[1;32m ██████  ███████ ██ ███████   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   ██  ██  ██     \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██      ██   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██████    ████      \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██ ███████   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     MMMMMMMM               MMMMMMMMPPPPPPPPPPPPPPPPP   HHHHHHHHH     HHHHHHHHHIIIIIIIIII   SSSSSSSSSSSSSSS HHHHHHHHH     HHHHHHHHH
+M:::::::M             M:::::::MP::::::::::::::::P  H:::::::H     H:::::::HI::::::::I SS:::::::::::::::SH:::::::H     H:::::::H
+M::::::::M          \e[0m''\e[1;33m\e[0m\e[1;32m
+ M::::::::MP::::::PPPPPP:::::P H:::::::H     H:::::::HI::::::::IS:::::SSSSSS::::::SH:::::::H     H:::::::H  \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     M:::::::::M M:::::::::MPP:::::P P:::::PHH::::::H H::::::HHII::::::IIS:::::S SSSSSSSHH::::::H H::::::HH M::::::::::M \e[0m''\e[1;33m\e[0m\e[1;32m M::::::::::M P::::P P:::::P H:::::H H:::::H I::::I S:::::S H:::::H H:::::H M:::::::::::M M:::::::::::M P::::P P:::::P H:::::H H:::::H I::::I S:::::S H:::::H H:::::H  \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     M:::::::M::::M M::::M:::::::M P::::PPPPPP:::::P H::::::HHHHH::::::H I::::I S::::SSSS H::::::HHHHH::::::H M::::::M M::::M M::::M M::::::M P:::::::::::::PP H:::::::::::::::::H I::::I SS::::::SSSSS H:::::::::::::::::H M::::::M M::::M::::M M::::::M P::::PPPPPPPPP H:::::::::::::::::H I::::I SSS::::::::SS H:::::::::::::::::H \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     M::::::M M:::::::M M::::::M P::::P H::::::HHHHH::::::H I::::I SSSSSS::::S H::::::HHHHH::::::H M::::::M M:::::M M::::::M P::::P H:::::H H:::::H I::::I S:::::S H:::::H H:::::H M::::::M MMMMM M::::::M P::::P H:::::H H:::::H I::::I S:::::S H:::::H H:::::H \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m    M::::::M MMMMM M::::::M P::::P H:::::H H:::::H I::::I S:::::S H:::::H H:::::H M::::::M M::::::MPP::::::PP HH::::::H H::::::HHII::::::IISSSSSSS S:::::SHH::::::H H::::::HH M::::::M M::::::MP::::::::P H:::::::H H:::::::HI::::::::IS::::::SSSSSS:::::SH:::::::H H:::::::H M::::::M M::::::MP::::::::P H:::::::H H:::::::HI::::::::IS:::::::::::::::SS H:::::::H H:::::::H MMMMMMMM MMMMMMMMPPPPPPPPPP HHHHHHHHH HHHHHHHHHIIIIIIIIII SSSSSSSSSSSSSSS HHHHHHHHH HHHHHHHHH  \e[0m'
   echo""    
-  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m OTP BYPASS PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.1] \e[0m'                                       
+  echo -e $'\e[1;33m\e[0m\e[1;33m    \e[0m'"\e[96m"'\e[1;33m\e[0m\e[1;31m\e[0m' '\e[1;32m\e[0m\e[1;32m OTP BYPASS PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.1] \e[0m'                                       
   echo ""
-   echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- https://github.com/Ignitetch/AdvPhishing \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- https://github.com/hackelite01/MPhish.git \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
    echo ""
       echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m'
-      echo -e "\e[93m    |A|D|V|A|N|C|E| |P|H|I|S|H|I|N|G| |2|.|1|"      
+      echo -e "\e[93m    |A|D|V|A|N|C|E| |P|H|I|S|H|I|N|G| |M|A|D|E| |B|Y| |M|A|Y|A|N|K|"      
       echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m' 
       echo ""                                                
       }
